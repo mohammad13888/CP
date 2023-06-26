cb36cc6ab41f13a9b29773290a53cec9bac8a416
Arshavir Shareghi <arshavirshareghi1387@gmail.com>
2023-06-26 18:38:59 +0330

    edition part 2

--- private/views.py
+++ private/views.py
@@ -98,12 +98,6 @@ def fetch_api(request):
     except:
         list_link=[]
         links=[]
-    try:
-        dislist=[]
-        for i in privates:
-            dislist.append(i.display_name)
-    except:
-    	dislist=[] 
     linak = []
     for i in links:
         if i.user1 == us:
@@ -116,9 +110,7 @@ def fetch_api(request):
         'nul':len(list_link),
         'channel':user_channels,
         'link_channel':channel_link,
-        'nuls':len(channel_link),
-        'dislist':dislist,
-        'dislen':len(dislist),
+        'nuls':len(channel_link)
     }
     return JsonResponse(data)
 def changer(old_path, id):
