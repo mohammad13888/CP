1f3bde436aadbaa9d36024dea6aab45694b07817
PashaMirzaei <mirzaeipasha8@gmail.com>
2023-08-19 11:37:24 +0330

    some fixings

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -25,7 +25,7 @@ DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 SECRET_KEY = 'django-insecure-!79n+mp+d)1-=z7w5ee9f5*74mhqbene^c*89j@(1gsot5&t@o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["0.0.0.0","127.0.0.1","192.168.1.101","192.168.1.100"]
 LOGOUT_REDIRECT_URL = '/'
--- djangochat/urls.py
+++ djangochat/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('core.urls')),
     path('chat/', include('private.urls')),
-
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
